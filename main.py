d7769d3eab69d5ed89d31dbd240fee847deda2b9
frgerold <franziskagerold987@gmail.com>
2024-07-29 14:36:46 +0200

    B: functionality: remove all spaces in the text

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     path_to_book = "books/frankenstein.txt"
     text = get_text(path_to_book)
     number_of_words = get_number_of_words(text)
-#    lowercase_text = make_lowercase(text)
+    lowercase_text = make_lowercase(text)
+    character_list = get_character_list(lowercase_text)
+    print(character_list)
     return print(f"The book contains {number_of_words} words.")
 
 def get_text(path):
@@ -15,12 +17,14 @@ def get_number_of_words(text):
     return len(words)
  
 
-#def make_lowercase(text):
-#    return text.lower()
+def make_lowercase(text):
+    return text.lower()
     
 
-#def get_word_list(lowercase_text):
-#    return lowercase_text.split()
+def get_character_list(text):
+    a = " ".join(text.split())
+    return a.replace(" ", "")
+
 
 #def get_characters(word_list):
 #    return word_list.split()
